7f7e1eb7e052a8c6219198d7f6f9d3e92f884295
leric434@gmail.com <leric434@gmail.com>
2021-08-24 10:10:59 -0700

    End of section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -8,7 +10,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanDeactivate } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -22,12 +24,16 @@ const routes: Routes = [
     children: [
       {
         path: 'members',
-        component: MemberListComponent,
-        canActivate: [AuthGuard]
+        component: MemberListComponent
       },
       {
         path: 'members/:username',
         component: MemberDetailComponent
+      },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [PreventUnsavedChangesGuard]
       },
       {
         path: 'lists',
---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,54 @@
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { MemberService } from './../../_services/member.service';
+import { AccountService } from './../../_services/account.service';
+import { Member } from './../../_models/member';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  member: Member;
+  user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(
+    private accountService: AccountService,
+    private memberService: MemberService,
+    private toastr: ToastrService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(
+      user => this.user = user
+    )
+  }
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.user.username).subscribe(
+      member => {
+        this.member = member;
+      }
+    )
+  }
+
+  updateMember() {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile Update Successfully');
+      this.editForm.reset(this.member);
+    });
+  }
+
+}
