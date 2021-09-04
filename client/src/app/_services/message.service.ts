82a9202d165b24d82adb9bd4ca97e7649818ca3b
leric434@gmail.com <leric434@gmail.com>
2021-09-04 12:37:35 -0700

    applied fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,3 +1,4 @@
+import { BusyService } from './busy.service';
 import { take } from 'rxjs/operators';
 import { User } from 'src/app/_models/user';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
@@ -21,9 +22,11 @@ export class MessageService {
 
   constructor(
     private http: HttpClient,
+    private busyService: BusyService
   ) { }
 
   createHubConnection(user: User, otherUsername: string) {
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
         accessTokenFactory: () => user.token
@@ -31,7 +34,9 @@ export class MessageService {
       .withAutomaticReconnect()
       .build();
 
-    this.hubConnection.start().catch(error => console.log(error));
+    this.hubConnection.start()
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
@@ -59,6 +64,7 @@ export class MessageService {
 
   stopHubConnection() {
     if (this.hubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
   }
